068904bb05a50d2e8f80406978d2b3b0dbb61fa3
Marek Połom <marekpolom22@gmail.com>
2020-11-16 18:50:52 +0100

    zadanie 2 test z poziomu klasy

--- tests/test_zad_1.py
+++ tests/test_zad_1.py
@@ -1,7 +1,7 @@
 import unittest
 
 from sample.zad_1 import IsPangram
-from parameterized import parameterized
+from parameterized import parameterized, parameterized_class
 
 class TestZad1(unittest.TestCase):
     def setUp(self):
@@ -30,6 +30,17 @@ class TestZad1(unittest.TestCase):
     def test_is_pangram(self, inp, exp):
         self.assertEqual(self.tmp.pangram(inp), exp)
 
+@parameterized_class(('inp', 'exp'), [
+        ("Pack my box with five dozen liquor jugs", 'To pangram'),
+        ("Jived fox nymph grabs quick waltz.", 'To pangram'),
+        ("12333211", 'To nie pangram'),
+])
+class TestZad1_2(unittest.TestCase):
+    def setUp(self):
+        self.tmp = IsPangram()
+
+    def test_is_pangram(self):
+        self.assertEqual(self.tmp.pangram(self.inp), self.exp)
 
 if __name__ == '__main__':
     unittest.main()
